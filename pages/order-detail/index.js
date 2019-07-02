b888ebaf95d9396d307caf8561075e6a8f73bf4b
davidpan123 <983411128@qq.com>
2019-07-02 23:37:59 +0800

    完善订单详情

--- pages/order-detail/index.js
+++ pages/order-detail/index.js
@@ -1,4 +1,4 @@
-// pages/order-detail/index.js
+const util = require('../../utils/util.js')
 Page({
 
   /**
@@ -6,19 +6,34 @@ Page({
    */
   data: {
     order: {
+      status: 1,
+      all_money: 344450.00,
+      logistics_money: 10,
+      rest_money: 344450.00,
       logistics: {
         info: {
           data: []
         }
-      }
-    }
+      },
+      goods: [
+        {
+          id: 1,
+          goods_img: '/images/order/icon_shop.png',
+          goods_name: 'CC卡美珠宝',
+          skuLabel: '2edds;ddddddddddddddddddddddddddddd;dddddddddddddddddddd;dddddddddddddd;',
+          goods_count: 2,
+          goods_price: 29000.90
+        }
+      ]
+    },
+    isShowPay: false,
+    statusName: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
 
   /**
@@ -32,7 +47,12 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    //created_at下单时间； finish_at取消时间； pay_time支付时间
+    // formatDate(this.data.order.created_at, 'yyyy-MM-dd hh:mm:ss')
+    this.setData({
+      statusName: this.getStatusName(this.data.order.status),
+      isShowPay: this.getOrderStatus(this.data.order.status)
+    })
   },
 
   /**
@@ -68,5 +88,28 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  getStatusName (status) {
+    const statusObjs = {
+      0: '待付款',
+      1: '待发货',
+      2: '待收货',
+      3: '已完成',
+      4: '退款中',
+      6: '已退款',
+      8: '已取消',
+    }
+    return statusObjs[status] || ''
+  },
+  getOrderStatus (status) {
+    if (status === 0) {
+      return false;
+    } else {
+      if (status === 8) {
+        return false;
+      } else {
+        return true;
+      }
+    }
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,43 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
+//格式化日期
+Date.prototype.format = function (fmt) {
+  let o = {
+    'M+': this.getMonth() + 1, //月份
+    'd+': this.getDate(), //日
+    'h+': this.getHours(), //小时
+    'm+': this.getMinutes(), //分
+    's+': this.getSeconds(), //秒
+    'q+': Math.floor((this.getMonth() + 3) / 3), //季度
+    S: this.getMilliseconds() //毫秒
+  };
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, String(this.getFullYear()).substr(4 - RegExp.$1.length));
+  }
+  for (let k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(String(o[k]).length));
+    }
+  }
+  return fmt;
+};
+
+//格式化日期
+const formatDate = (nS, format) => {
+  //日期格式化
+  if (!nS) {
+    return '';
+  }
+  format = format || 'yyyy-MM-dd hh:mm:ss';
+  if (typeof nS == 'string') {
+    nS = nS.replace(/-/g, '/');
+    if (nS.indexOf('.') != -1) {
+      nS = nS.substr(0, nS.indexOf('.'));
+    }
+  }
+  return new Date(nS).format(format);
 }
 
 module.exports = {
-  formatTime: formatTime
+  formatDate: formatDate
 }
